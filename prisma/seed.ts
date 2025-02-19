6cf7be96d4ee0402d7fb5b772392ed5144328e56
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-02-19 20:07:38 +0200

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,20 +5,33 @@ const prisma = new PrismaClient();
 async function main() {
     // Create example users
 
-    const arrayBuffer = await crypto.subtle.digest(
-        "SHA-512",
-        new TextEncoder().encode("Mamdouh123!!!")
-    )
+    // const arrayBuffer = await crypto.subtle.digest(
+    //     "SHA-512",
+    //     new TextEncoder().encode("Mamdouh123!!!")
+    // )
 
-    const pass = Buffer.from(arrayBuffer).toString("base64")
+    // const pass = Buffer.from(arrayBuffer).toString("base64")
 
-    await prisma.users.createMany({
-        data: [
-            { email: 'admin@gmail.com', password: pass },
-        ],
+    // await prisma.users.createMany({
+    //     data: [
+    //         { email: 'admin@gmail.com', password: pass },
+    //     ],
+    // });
+
+    await prisma.branches.updateMany({
+        where: {
+            longitude: undefined,
+            latitude: undefined
+        },
+        data: {
+            latitude: 1,
+            longitude: 1,
+        },
     });
 
-    console.log('Seed data inserted');
+    console.log('Updated empty latitude values to NULL');
+
+    // console.log('Seed data inserted');
 }
 
 main()
